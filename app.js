5415dbc5e7d1eba1edcbe80d05fb0a22d041ce56
wangyhub <459152081@qq.com>
2019-06-04 11:00:25 +0800

    修改

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 //app.js
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    //var logs = wx.getStorageSync('logs') || []
-    //logs.unshift(Date.now())
-    //wx.setStorageSync('logs', logs)
-    if(wx.getStorageSync('isLogin')){
+    
+    /**if(wx.getStorageSync('isLogin')){
       wx.switchTab({
         url: 'pages/meeting/meeting',
       })
@@ -14,36 +11,50 @@ App({
         url: 'pages/login/login',
       })
     }
+    */
+
+    var that = this;
     // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        console.log(res.code);
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+        if (res.code!=null && res.code!=''){
+          //获取用户微信信息
           wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
+            success: function (res) {
+              wx.setStorageSync('userInfo', res.userInfo);//存储userInfo
             }
           })
+          var d = that.globalData;//这里存储了appid、secret、token串  
+          console.log("d.appId=" + d.appId + "d.secret=" + d.secret);
+          var l = 'https://api.weixin.qq.com/sns/jscode2session?appid=' + d.appId + '&secret=' + d.secret + '&js_code=' + res.code + '&grant_type=authorization_code';
+          wx.request({
+            url: l,
+            data: {},
+            method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT  
+            // header: {}, // 设置请求的 header  
+            success: function (res) {
+              var obj = {};
+              obj.openid = res.data.openid;
+              obj.expires_in = Date.now() + res.data.expires_in;
+              obj.unionid = res.data.unionid;
+              console.log("res.data=" + res.data);
+              console.log("obj.openid=" + obj.openid + " obj.expires_in=" + obj.expires_in + " obj.unionid=" + obj.unionid);
+              //console.log(obj);
+              //wx.setStorageSync('user', obj);//存储openid  
+            }
+          });
+        }else{
+          console.log('获取用户登录态失败！' + res.errMsg);
         }
       }
     })
+    
   },
   globalData: {
     userInfo: null,
-    url: "http://39.155.223.208:9080/meeting/a/meeting/mobile/tbMobile"
+    url: "http://39.155.223.208:9080/meeting/a/meeting/mobile/tbMobile",
+    appId: 'wx478373945bfbbb7c',
+    secret: '74cf7d9bbd4252dce010b716d379f9e7'
   }
 })
--- pages/material/material.js
+++ pages/material/material.js
@@ -5,62 +5,90 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    file:{
+      fileName: '',
+      fileId: ''
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var url = getApp().globalData.url;
+    var id = wx.getStorageSync('meetingId');
+    console.log("meetingId="+id);
+    var that = this;
+    wx.request({
+      url: url +'/findFileList?callback=JSONP_CALLBACK',
+      data: {
+        meetId: id
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success(res){
+        var data = JSON.parse(res.data.replace('JSONP_CALLBACK(', '').replace('})', '}'));
+        var entity = data.data;
+        var newFileName;
+        var newName = [];
+        for (var i in entity) {        //遍历JSON数组
+          let name = entity[i].filename;     //获取每个文件的名字
+          let id = entity[i].fileId;
+          if (name.indexOf(".") != -1) {       //判断名字是否包括.这个字符串
+            var first = name.split(".")[0];  //截取前面的文件名
+            var last = name.split(".")[1];  //截取后面的文件类型
+            if (first.length >= 16) {
+              var firstName = first.substr(0, 14);
+              newFileName = firstName + "...." + last;
+            } else {
+              newFileName = name;
+            }
+            newName.push({ 'fileName': newFileName, 'fileId': id });
+          }
+        }
+        that.setData({
+          file: newName
+        })
+      },
+      fail(err){
+        console.log("err="+err);
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  downLoad: function(event){
+    var fileId = event.target.dataset.id;
+    const downloadTask = wx.downloadFile({
+      url: getApp().globalData.url + '/fileDownLoad?id=' + fileId,
+      success: function(res){
+        var filePath = res.tempFilePath;
+        // 查看下载的文件列表
+        //wx.getSavedFileList({
+        //  success: function (res) {
+        //    console.log(res.fileList);
+        //  }
+        //})
+        // 打开文档
+        wx.openDocument({
+          filePath: filePath,
+          fileType: 'docx',//必须指定查看文件类型
+          success: function (res) {
+            console.log('打开文档成功')
+          },
+          fail: function (res) {
+            console.log(res)
+          }
+        })
+      },
+      fail: function (res) {
+        console.log(res)
+      }
+    })
+    downloadTask.onProgressUpdate((res) => {
+      //console.log('下载进度', res.progress)
+      //console.log('已经下载的数据长度', res.totalBytesWritten)
+      //console.log('预期需要下载的数据总长度', res.totalBytesExpectedToWrite)
+    })
   }
 })
